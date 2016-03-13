6311815aa770f0196de5be7bc7837b6ebe9d39f8
Ravichandra-C <ravichandraraviraviravi@gmail.com>
2016-03-14 00:45:32 +0530

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -1,4 +1,4 @@
-from math import sqrt,ceil 
+from math import sqrt,floor
 import time
 
 
@@ -8,15 +8,16 @@ num=10001
 i=3
 while len(list_1)<num:
     prime=True
-    count_i=sqrt(i)+1
+    count_i=int(sqrt(i)+1)
     #print (count_i)
-    for j in list_1:
-        if j<count_i and i%j==0:
+    #for j in list_1:
+    for j in range(2,count_i):
+        if  i%j==0:
             prime=False
             break
+    
     if prime==True:
         list_1.append(i)
     i+=2
 print list_1.pop()
 print "Time taken to complete:",time.clock()-start
-
